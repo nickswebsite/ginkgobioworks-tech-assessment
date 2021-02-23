f65478fb14cebe9bad06cb75f01b9fea40f1debf
nrobinson <nrobinson@transparent.com>
2021-02-23 14:19:09 +0000

    Specified LOGIN_URL, LOGIN_REDIRECT_URL, and LOGOUT_REDIRECT_URL.

--- conf/settings.py
+++ conf/settings.py
@@ -122,6 +122,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Login
+LOGIN_URL = "/login"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/login"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
