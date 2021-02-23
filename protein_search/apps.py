37181507885cd276d44384ba67396c03370c6960
nrobinson <nrobinson@transparent.com>
2021-02-23 14:17:01 +0000

    Throw an exception if `ENTREZ_EMAIL` is not specified in settings.py.

--- protein_search/apps.py
+++ protein_search/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 
 class ProteinSearchConfig(AppConfig):
     name = 'protein_search'
+
+    def ready(self):
+        if not hasattr(settings, "ENTREZ_EMAIL") or not settings.ENTREZ_EMAIL:
+            raise ImproperlyConfigured("`ENTREZ_EMAIL` MUST be specified in your settings file.")
